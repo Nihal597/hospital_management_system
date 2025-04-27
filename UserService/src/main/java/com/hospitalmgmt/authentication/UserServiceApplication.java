b5e3cf3991a98d3af2ecffbae7f6513a127a2d19
Nihal597 <nihal1491@gmail.com>
2025-04-27 19:38:09 +0530

    user-service docker file changes

--- UserService/src/main/java/com/hospitalmgmt/authentication/UserServiceApplication.java
+++ UserService/src/main/java/com/hospitalmgmt/authentication/UserServiceApplication.java
@@ -1,13 +1,23 @@
 package com.hospitalmgmt.authentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
+	@Autowired
+	DataSource dataSource;
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
 }
